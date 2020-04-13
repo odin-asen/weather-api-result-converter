024b2b38d94d90eb4b0e8212a968b14aa2847952
odin-asen <timm.herrmann@freenet.de>
2020-04-13 19:26:02 +0200

    Merge static api to weather_api_forecast.py

--- weather-api-result-converter/forecast_api/weather_api_forecast.py
+++ weather-api-result-converter/forecast_api/weather_api_forecast.py
@@ -7,18 +7,25 @@ from request_query_string_parser import RequestQueryStringParser
 
 
 class WeatherAPIForecast(BaseAPIForecast):
-    def __init__(self, query_string_parser: RequestQueryStringParser):
+    def __init__(self, query_string_parser: RequestQueryStringParser, use_static_file=False):
         super().__init__(query_string_parser)
-        if not query_string_parser.has_api_key() or not query_string_parser.has_search_query():
-            raise ValueError('Query string contains insufficient set of values. '
-                             'WeatherAPIForecast requires api key and search query')
+        self.use_static_file = use_static_file
+        if not use_static_file:
+            if not query_string_parser.has_api_key() or not query_string_parser.has_search_query():
+                raise ValueError('Query string contains insufficient set of values. '
+                                 'WeatherAPIForecast requires api key and search query')
 
     def retrieve_json_string_from_endpoint(self):
-        response = requests.get(self.make_forecast_url())
-        if response.status_code == 200:
-            return response.text
+        if self.use_static_file:
+            static_file_path = 'resources/weather-api/forecast-singen.json'
+            with open(static_file_path, 'r') as forecast_json_file:
+                return forecast_json_file.read()
+        else:
+            response = requests.get(self.make_forecast_url())
+            if response.status_code == 200:
+                return response.text
 
-        raise ValueError('Endpoint did not return OK, instead:', response, response.text)
+            raise ValueError('Endpoint did not return OK, instead:', response, response.text)
 
     def make_forecast_url(self):
         return 'http://api.weatherapi.com/v1/forecast.json?key={api_key}&q={query}&days={days:d}'\
--- weather-api-result-converter/main.py
+++ weather-api-result-converter/main.py
@@ -1,21 +1,19 @@
 import cgitb
 import cgi_parameters
-from forecast_api import weather_api_forecast, static_api_forecast
+from forecast_api import weather_api_forecast
 from request_query_string_parser import RequestQueryStringParser
 
-USE_API_ENDPOINT = True
+USE_STATIC_FILE = True
 
 
 def main():
     weather_api_query_string = cgi_parameters.get_cgi_url_query_string()
     query_parser = RequestQueryStringParser(weather_api_query_string)
 
-    if not USE_API_ENDPOINT:
+    if USE_STATIC_FILE:
         query_parser.parsed_query_dict['format'] = 'json'
-        api_forecast = static_api_forecast.StaticAPIForecast(query_parser)
-    else:
-        api_forecast = weather_api_forecast.WeatherAPIForecast(query_parser)
 
+    api_forecast = weather_api_forecast.WeatherAPIForecast(query_parser, USE_STATIC_FILE)
     print(api_forecast.fetch_and_map_response_as_string())
 
 
